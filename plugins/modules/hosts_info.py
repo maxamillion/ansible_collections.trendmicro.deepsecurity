7d5c1ae55721c8ce64271701d839235b8e00f16c
Adam Miller <admiller@redhat.com>
2020-03-14 00:02:11 -0500

    WIP - added the connection plugin, module_utils, and a test module ... doesn't work yet but we'll get there
    Signed-off-by: Adam Miller <admiller@redhat.com>

---
+++ plugins/modules/hosts_info.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2019, Adam Miller (admiller@redhat.com)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {
+    "metadata_version": "1.1",
+    "status": ["preview"],
+    "supported_by": "community",
+}
+DOCUMENTATION = """
+---
+module: hosts_info
+short_description: Obtain information about one or many Hosts defined by TrendMicro Deep Security
+description:
+  - This module obtains information about Hosts defined by TrendMicro Deep Security
+version_added: "2.9"
+options:
+  id:
+    description:
+      - Obtain only information of the Rule with provided ID
+    required: false
+    type: int
+
+author: Ansible Security Automation Team (@maxamillion) <https://github.com/ansible-security>"
+"""
+
+
+# FIXME - provide correct example here
+RETURN = """
+"""
+
+EXAMPLES = """
+"""
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils._text import to_text
+
+from ansible.module_utils.urls import Request
+from ansible.module_utils.six.moves.urllib.parse import quote
+from ansible.module_utils.six.moves.urllib.error import HTTPError
+from ansible_collections.trendmicro.deepsecurity.plugins.module_utils.deepsecurity import DeepSecurityRequest
+
+import copy
+import json
+
+
+def main():
+
+    argspec = dict(
+        id=dict(required=False, type="int"),
+    )
+
+    module = AnsibleModule(argument_spec=argspec, supports_check_mode=True)
+
+    deepsec_request = DeepSecurityRequest(
+        module, headers={"Content-Type": "application/json"}
+    )
+
+    hosts = deepsec_request.get('/rest/hosts')
+
+    module.exit_json(hosts=hosts, changed=False)
+
+
+if __name__ == "__main__":
+    main()
